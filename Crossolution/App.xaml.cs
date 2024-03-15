b52172daffdb5e94d7450650112d80ee2a9bb02b
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2024-03-14 23:23:22 -0300

    initial commit

---
+++ Crossolution/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Crossolution
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
