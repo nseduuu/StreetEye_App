4ae9862fe4553f3bf3d72a845c2649dcc45db948
Eduardo Noronha <eduardo.noronhasampaio@gmail.com>
2024-04-28 13:57:35 -0300

    feat: adding view cadastro

--- Crossolution/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Crossolution.AssemblyInfo.cs
+++ Crossolution/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/Crossolution.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Crossolution")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+72b41b7adde2ef056a5ebd2cf90e665f678bd898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+2af76184d9155ede36e9a4273c7958197b7b3a0b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Crossolution")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Crossolution")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
